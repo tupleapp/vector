a6fa49f2f63040415c586bb76d1026208d0ebbd1
Toby Lawrence <toby@nuclearfurnace.com>
2021-10-07 22:25:16 -0400

    wip
    Signed-off-by: Toby Lawrence <toby@nuclearfurnace.com>

--- src/config/datadog.rs
+++ src/config/datadog.rs
@@ -1,6 +1,6 @@
 use super::{ComponentKey, Config, OutputId, SinkOuter, SourceOuter};
 use crate::{
-    sinks::datadog::metrics::DatadogConfig, sources::internal_metrics::InternalMetricsConfig,
+    sinks::datadog::metrics::DatadogMetricsConfig, sources::internal_metrics::InternalMetricsConfig,
 };
 use serde::{Deserialize, Serialize};
 use std::env;
@@ -75,7 +75,7 @@ pub fn try_attach(config: &mut Config) -> bool {
     );
 
     // Create a Datadog metrics sink to consume and emit internal + host metrics.
-    let datadog_metrics = DatadogConfig::from_api_key(api_key);
+    let datadog_metrics = DatadogMetricsConfig::from_api_key(api_key);
 
     config.sinks.insert(
         datadog_metrics_id,
